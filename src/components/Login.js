ea3a02d526f71c481dd4196c33594b06f36df255
luchen <luchenliu929@gmail.com>
2019-08-10 19:08:49 -0600

    login validation update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Form, Icon, Input, Button, message } from 'antd';
 import  { Link } from 'react-router-dom';
+import { API_ROOT } from '../constants'; 
 
 class NormalLoginForm extends React.Component {
   handleSubmit = e => {
@@ -9,6 +10,28 @@ class NormalLoginForm extends React.Component {
       if (!err) {
         console.log('Received values of form: ', values);
       }
+      fetch(`${API_ROOT}/login`, {
+            method: 'POST',
+            body: JSON.stringify ({
+                username: values.username,
+                password: values.password,
+            })
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.text()
+            } 
+            throw new Error(response.statusText);
+        })
+        .then((data) => {
+            console.log(data)
+            message.success('Login Succeed!')
+            
+        }) 
+        .catch((err) => {
+            console.log(err)
+            message.error('Login Failed.')
+        } )
     });
   };
 
